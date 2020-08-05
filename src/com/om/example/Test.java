6ffb5a24fb2668ec31b8203b8cc43f2b747dfef2
atkuzmanov <atkuzmanov@gmail.com>
2020-08-05 11:28:29 +0300

    Work-In-Progress-WIP-2020-08-05-112829

---
+++ src/com/om/example/Test.java
@@ -0,0 +1,14 @@
+package com.om.example;
+
+public class Test {
+
+	public static void main(String[] args){
+		int x = 3;
+		int r = 9;
+		int y = 9;
+		r -= x; // r = 9 - 3 = 6
+		y =- x; // r = (-) 3 = assigning -3 to y
+		System.out.println("r = " + r);
+		System.out.println("y = " + y);
+	}
+}
